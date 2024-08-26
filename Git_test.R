115680b702d6115f490f0c9214554b368ca3a128
boumae <reddelta002@gmail.com>
2024-08-26 15:39:46 -0400

    Git test

---
+++ Git_test.R
@@ -0,0 +1,9 @@
+#commandline auth
+
+C:\Users\redde>git config --global user.name "boumae"
+
+C:\Users\redde>git config --global user.email "reddelta002@gmail.com"
+
+C:\Users\redde>git config --global --list
+user.name=boumae
+user.email=reddelta002@gmail.com
