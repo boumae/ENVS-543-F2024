9d7368ce61d057f38d979580fd30364257de9c3e
boumae <reddelta002@gmail.com>
2024-08-26 16:01:44 -0400

    removed more info

--- Git_test.R
+++ Git_test.R
@@ -5,5 +5,5 @@ C:\Users\redde>git config --global user.name "github_user"
 C:\Users\redde>git config --global user.email "your.email@blank.com"
 
 C:\Users\redde>git config --global --list
-user.name=boumae
-user.email=reddelta002@gmail.com
+user.name=github_user
+user.email=your.email@blank.com
