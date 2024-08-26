d75677e6daf6f29555aa83af8f03ed97ac570d13
boumae <reddelta002@gmail.com>
2024-08-26 15:57:14 -0400

    removed info

--- Git_test.R
+++ Git_test.R
@@ -1,8 +1,8 @@
 #commandline auth
 
-C:\Users\redde>git config --global user.name "boumae"
+C:\Users\redde>git config --global user.name "github_user"
 
-C:\Users\redde>git config --global user.email "reddelta002@gmail.com"
+C:\Users\redde>git config --global user.email "your.email@blank.com"
 
 C:\Users\redde>git config --global --list
 user.name=boumae
